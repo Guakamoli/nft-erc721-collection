d682e18163b3684c95a5dd90e0c20979479299a8
Wang Wenlin <sopl.wang@gmail.com>
2022-12-10 15:34:48 +0800

    improve: add airdrop batch mint script

---
+++ smart-contract/scripts/9_airdrop.ts
@@ -0,0 +1,22 @@
+import NftContractProvider from '../lib/NftContractProvider';
+import airdropAddresses from '../config/airdrops.json';
+
+async function main() {
+  // Attach to deployed contract
+  const contract = await NftContractProvider.getContract();
+
+  // Airdrop mints
+  for (let i = 0; i < airdropAddresses.length; i += 100) {
+    let batchAddrs = airdropAddresses.slice(i, i + 100);
+    await (await contract.mintForAddress(1, batchAddrs)).wait();
+  }
+
+  console.log('Your collection is airdroped!');
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
