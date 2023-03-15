fd7d4ad734448d34b414a72b89239820989d0449
Wang Wenlin <sopl.wang@gmail.com>
2023-03-15 16:15:25 +0800

    fix: Remix IDE compile error on import Strings

--- smart-contract/contracts/YourNftToken.sol
+++ smart-contract/contracts/YourNftToken.sol
@@ -5,6 +5,7 @@ pragma solidity >=0.8.9 <0.9.0;
 import 'erc721a/contracts/extensions/ERC721AQueryable.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/utils/cryptography/MerkleProof.sol';
+import '@openzeppelin/contracts/utils/Strings.sol';
 import './Withdrawable.sol';
 import './Royalty.sol';
 
